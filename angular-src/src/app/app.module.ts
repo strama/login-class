a4de7117ddfe1b78781295f7b293c92bcc8e3560
Victor Kazuyuki Shinya <contatovictorshinya@gmail.com>
2017-11-22 00:50:54 -0200

    Home page configured

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
 
+const appRoot = [
+  { path: '', component: HomeComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoot)
   ],
   providers: [],
   bootstrap: [AppComponent]
