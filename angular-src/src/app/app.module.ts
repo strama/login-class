b74864bd70817e70fee6795480dcb76f0337b9ed
Victor Kazuyuki Shinya <contatovictorshinya@gmail.com>
2018-01-10 23:53:23 -0200

    Changed layout page

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -8,7 +8,6 @@ import { FlashMessagesModule } from 'angular2-flash-messages/module';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
@@ -28,7 +27,6 @@ const appRoot = [
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent,
     HomeComponent,
     LoginComponent,
     RegisterComponent,
--- angular-src/src/app/components/home/home.component.ts
+++ angular-src/src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -7,13 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
-
-  // apis = [
-  //   { route: '/api/user/login', name: 'POST Login' },
-  //   { route: '/api/user/register', name: 'POST Register' },
-  //   { route: '/api/user/auth', name: 'POST Auth' }
-  // ];
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
   }
